149dfff1799f8d6f4c2ce4930b7431a9ab9b87cf
qeude <quentineude@gmail.com>
2020-07-12 19:09:02 +0200

    ✨ Added Day34 - Added GuessTheFlag animations challenges

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -15,6 +15,10 @@ struct ContentView: View {
 
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var rotationAmount = 0.0
+    @State private var opacityAmount = 1.0
+    @State private var showRedCross = [false, false, false]
+    @State private var redCrossAnimationAmount: CGFloat = 1.0
 
     var body: some View {
         ZStack {
@@ -32,14 +36,25 @@ struct ContentView: View {
                 }
 
                 ForEach(0 ..< 3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                    }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(RoundedRectangle(cornerRadius: 35))
-                            .shadow(color: Color(red: 60/255, green: 60/255, blue: 60/255, opacity: 1.0), radius: 3)
-//                            .overlay(RoundedRectangle(cornerRadius: 35).stroke(Color.white, lineWidth: 1))
+                    Group {
+                        if showRedCross[number] {
+                            flag(for: number)
+                                .overlay(
+                                    Image(systemName: "xmark.square")
+                                        .foregroundColor(.red)
+                                        .font(.system(size: 60, weight: .bold, design: .default))
+                                        .scaleEffect(redCrossAnimationAmount)
+                                        .animation(
+                                            Animation.easeOut(duration: 1)
+                                                .repeatForever(autoreverses: true)
+                                        )
+                                )
+                                .onAppear {
+                                    self.redCrossAnimationAmount = 1.5
+                                }
+                        } else {
+                            flag(for: number)
+                        }
                     }
                 }
 
@@ -55,14 +70,38 @@ struct ContentView: View {
         }
     }
 
+    func flag(for number: Int) -> some View {
+        return Button(action: {
+            self.flagTapped(number)
+        }) {
+            Image(self.countries[number])
+                .renderingMode(.original)
+                .clipShape(RoundedRectangle(cornerRadius: 35))
+                .shadow(color: Color(red: 60/255, green: 60/255, blue: 60/255, opacity: 1.0), radius: 3)
+        }
+        .rotation3DEffect(.degrees(number == self.correctAnswer ? self.rotationAmount : 0), axis: (x: 0, y: 1, z: 0))
+        .opacity(number != self.correctAnswer ? self.opacityAmount : 1.0)
+    }
+
     func flagTapped(_ number: Int) {
+        withAnimation {
+            opacityAmount = 0.25
+        }
         if number == correctAnswer {
             currentScore += 1
             scoreTitle = "Correct"
             scoreMessage = "Well done ! Your score is \(currentScore)"
+
+            withAnimation {
+                rotationAmount = 360
+            }
         } else {
             scoreTitle = "Wrong"
             scoreMessage = "Wrong, That's the flag of \(countries[number])"
+
+            withAnimation {
+                showRedCross[number] = true
+            }
         }
 
         showingScore = true
@@ -71,8 +110,16 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        withAnimation {
+            self.opacityAmount = 1.0
+        }
+        self.rotationAmount = 0
+        showRedCross = [false, false, false]
+        redCrossAnimationAmount = 1.0
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
